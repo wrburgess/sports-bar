be14161f11f32258460c46c409432cc0311c60f9
Randy Burgess <wrburgess@wrburgess.com>
2013-10-05 19:16:28 -0500

    adding layouts and location controller specs

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -2,15 +2,42 @@ require 'spec_helper'
 
 describe LocationsController do
 
-  it 'should have an index' do
-    get :index
-    response.should be_success
+  let(:location1) { FactoryGirl.create(:location) }
+  let(:location2) { FactoryGirl.create(:location) }
+
+  describe "#index" do
+    it 'should have an index' do
+      location1
+      location2
+      get :index
+      response.should be_success
+    end
+
+    it "assigns the requested players to @players" do
+      locations = Location.all
+      get :index
+      expect(assigns(locations)).to eq @locations
+    end
   end
 
-  it 'should have a show' do
-    location = FactoryGirl.create :location
-    get :show, id: location 
-    response.should be_success
+  describe "#show" do
+    it 'should have a show' do
+      location1
+      get :show, id: location1 
+      response.should be_success
+    end
+
+    it "renders the show view" do
+      location1
+      get :show, id: location1
+      response.should render_template :show
+    end
+
+    it "assigns the requested location to @location" do
+      location1
+      get :show, id: location1
+      expect(assigns(location1)).to eq @location
+    end
   end
 
 end
